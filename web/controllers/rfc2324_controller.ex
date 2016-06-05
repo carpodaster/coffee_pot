235b038ea46fb6c69d59b6d28a35b7ce35dd4618
Carsten Zimmermann <cz@aegisnet.de>
2016-06-05 20:52:11 +0200

    Add verbs as comments

--- web/controllers/rfc2324_controller.ex
+++ web/controllers/rfc2324_controller.ex
@@ -1,18 +1,22 @@
 defmodule CoffeePot.Rfc2324Controller do
   use CoffeePot.Web, :controller
 
+  # GET /coffee
   def index(conn, _params) do
     text conn, "Ask #{CoffeePot.Consumer.sample} to pour you another cup.\n"
   end
 
+  # PROPFIND /coffee
   def options(conn, _params) do
     json conn, %{ drinkers: CoffeePot.Consumer.all }
   end
 
+  # BREW /coffee
   def brew(conn, _params) do
     text conn, "It's #{CoffeePot.Consumer.sample}'s turn to brew it!\n"
   end
 
+  # WHEN /coffee
   def not_implemented(conn, _params) do
     send_resp conn, 501, "Not Implemented\n"
   end
